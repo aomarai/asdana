a8b6ed03091d14c14be880464fc3bd0a1a70b374
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2024-11-07 18:23:13 -0600

    Trying to fix workflows still
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to fix workflows still
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Trying to work around UTF-8 + black formatting
    
    Updating to checkoutv4

--- asdana/cogs/members/members.py
+++ asdana/cogs/members/members.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 
+
 class Members(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-
--- asdana/main.py
+++ asdana/main.py
@@ -4,7 +4,7 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
-BOT_NAME = 'asdana'
+BOT_NAME = "asdana"
 
 # Load environment variables
 load_dotenv()
@@ -17,28 +17,36 @@ intents.members = True
 intents.message_content = True
 
 # Create the bot instance
-bot = commands.Bot(command_prefix=['!', '?', '$'], intents=intents, description=DESCRIPTION, case_insensitive=True)
+bot = commands.Bot(
+    command_prefix=["!", "?", "$"],
+    intents=intents,
+    description=DESCRIPTION,
+    case_insensitive=True,
+)
+
 
 # Dynamically load available cogs
 async def load_cogs():
-    cogs_dir = os.path.join('cogs')
+    cogs_dir = os.path.join("cogs")
     for root, _, files in os.walk(cogs_dir):
-        if root == 'cogs':
+        if root == "cogs":
             continue
 
-        if '__init__.py' in files:
-            cog_path = root.replace(os.path.sep, '.')
+        if "__init__.py" in files:
+            cog_path = root.replace(os.path.sep, ".")
             try:
                 await bot.load_extension(cog_path)
-                print(f'Loaded Cog {cog_path[5:]}')
+                print(f"Loaded Cog {cog_path[5:]}")
             except Exception as e:
-                print(f'Failed to load Cog {cog_path[5:]}: {e}')
+                print(f"Failed to load Cog {cog_path[5:]}: {e}")
+
 
 # Start the bot instance
 @bot.event
 async def on_ready():
     await load_cogs()
-    print(f'Logged into bot user {bot.user} (ID: {bot.user.id})')
-    print('----------')
+    print(f"Logged into bot user {bot.user} (ID: {bot.user.id})")
+    print("----------")
+
 
 bot.run(TOKEN)
