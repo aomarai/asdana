565e9242db2855fbd62120324ecdc85fda648c4e
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2024-11-08 21:35:22 -0600

    Adding random youtube video functionality via YT video DB

--- asdana/cogs/youtube/youtube.py
+++ asdana/cogs/youtube/youtube.py
@@ -1,7 +1,10 @@
 """
 Provides commands and listeners for YouTube-based functionality and other utilities.
 """
+
 import os
+import asyncpg
+from asdana.postgres.connection import PostgresConnection
 from discord.ext import commands
 from googleapiclient.discovery import build
 
@@ -29,7 +32,7 @@ class YouTube(commands.Cog):
         :return: The search results.
         """
         youtube = self.__get_youtube_service()
-        request = youtube.search().list( # pylint: disable=no-member
+        request = youtube.search().list(  # pylint: disable=no-member
             part="snippet",
             maxResults=1,
             q=query,
@@ -38,10 +41,51 @@ class YouTube(commands.Cog):
         response = request.execute()
         return response
 
-    def __build_url_from_id(self, video_id: str):
+    async def __get_random_video_id_from_db(
+        self, connection: asyncpg.Connection = None
+    ):
+        """
+        Gets a random video ID from the YouTube Video ID database.
+        :return: A random video ID.
+        """
+        # Connect to the db
+        if connection is None:
+            connection = PostgresConnection()
+            await connection.connect(
+                host=os.getenv("YT_PG_HOST"),
+                database=os.getenv("YT_PG_DATABASE"),
+                user=os.getenv("YT_PG_USER"),
+                password=os.getenv("YT_PG_PASSWORD"),
+                port=os.getenv("YT_PG_PORT"),
+            )
+
+        # Grab a random video ID
+        query = "SELECT video_id FROM youtube_videos ORDER BY RANDOM() LIMIT 1;"
+        video_id = await connection.fetchval(query)
+        return video_id
+
+    def __build_url_from_id(
+        self, video_id: str
+    ):  # pylint: disable=unused-private-member
         """
         Builds a YouTube video URL from a video ID.
         :param video_id: The ID of the video.
         :return: The URL of the video.
         """
         return f"https://www.youtube.com/watch?v={video_id}"
+
+    @commands.command(name="randyt", aliases=["ryt"])
+    async def random_you_tube_video(self, context: commands.Context):
+        """
+        Selects a random video from YouTube. Searches for a random video ID in the database and sends the URL.
+        :param context: The context of the command.
+        :return: None
+        """
+        # Get a random video ID
+        video_id = await self.__get_random_video_id_from_db()
+
+        # Build the URL
+        video_url = self.__build_url_from_id(video_id)
+
+        # Send the URL
+        await context.send(f"Your random video is: {video_url}")
